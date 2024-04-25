a7b175d6874b0746b0dec56fa320a2ccc4b54165
walid938 <w.seghdau@gmail.com>
2024-04-25 22:05:31 +0200

    Update TourGuideController to Return JSON Object for Nearby Attractions

--- TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
@@ -1,7 +1,12 @@
 package com.openclassrooms.tourguide;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.openclassrooms.tourguide.model.attraction.AttractionInfo;
+import gpsUtil.location.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,22 +36,35 @@ public class TourGuideController {
     public VisitedLocation getLocation(@RequestParam String userName) {
     	return tourGuideService.getUserLocation(getUser(userName));
     }
-    
-    //  TODO: Change this method to no longer return a List of Attractions.
- 	//  Instead: Get the closest five tourist attractions to the user - no matter how far away they are.
- 	//  Return a new JSON object that contains:
-    	// Name of Tourist attraction, 
-        // Tourist attractions lat/long, 
-        // The user's location lat/long, 
-        // The distance in miles between the user's location and each of the attractions.
-        // The reward points for visiting each Attraction.
-        //    Note: Attraction reward points can be gathered from RewardsCentral
-    @RequestMapping("/getNearbyAttractions") 
-    public List<Attraction> getNearbyAttractions(@RequestParam String userName) {
-    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
-    	return tourGuideService.getNearByAttractions(visitedLocation);
+
+    //  Return a new JSON object that contains:
+    // Name of Tourist attraction,
+    // Tourist attractions lat/long,
+    // The user's location lat/long,
+    // The distance in miles between the user's location and each of the attractions.
+    // The reward points for visiting each Attraction.
+    @RequestMapping("/getNearbyAttractions")
+    public Map<String, Object> getNearbyAttractions(@RequestParam String userName) {
+        Map<String, Object> nearbyAttractions = new HashMap<>();
+        User user = tourGuideService.getUser(userName);
+
+        VisitedLocation visitedLocation = tourGuideService.getUserLocation(user);
+        List<Attraction> fiveNearestAttractions = tourGuideService.getNearByAttractions(visitedLocation);
+
+        List<AttractionInfo> attractionInfoList = new ArrayList<>();
+        for (Attraction attraction : fiveNearestAttractions) {
+            attractionInfoList.add(
+                    tourGuideService.attractionInfoBuilder(attraction, user));
+        }
+        nearbyAttractions.put("attraction information", attractionInfoList);
+
+        Location userLocation = visitedLocation.location;
+        nearbyAttractions.put("user location", userLocation);
+
+        return nearbyAttractions;
     }
-    
+
+
     @RequestMapping("/getRewards") 
     public List<UserReward> getRewards(@RequestParam String userName) {
     	return tourGuideService.getUserRewards(getUser(userName));
