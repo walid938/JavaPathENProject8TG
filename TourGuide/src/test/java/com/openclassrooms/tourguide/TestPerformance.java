5887ea156770bd495b00ad10a19db94c9708c526
walid938 <w.seghdau@gmail.com>
2024-04-25 21:35:03 +0200

    Refactor Performance Tests for Improved Efficiency and Readability

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
@@ -5,11 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.time.StopWatch;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Disabled;
 
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
@@ -24,18 +26,18 @@ public class TestPerformance {
 
 	/*
 	 * A note on performance improvements:
-	 * 
+	 *
 	 * The number of users generated for the high volume tests can be easily
 	 * adjusted via this method:
-	 * 
+	 *
 	 * InternalTestHelper.setInternalUserNumber(100000);
-	 * 
-	 * 
+	 *
+	 *
 	 * These tests can be modified to suit new solutions, just as long as the
 	 * performance metrics at the end of the tests remains consistent.
-	 * 
+	 *
 	 * These are performance metrics that we are trying to hit:
-	 * 
+	 *
 	 * highVolumeTrackLocation: 100,000 users within 15 minutes:
 	 * assertTrue(TimeUnit.MINUTES.toSeconds(15) >=
 	 * TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
@@ -52,17 +54,25 @@ public class TestPerformance {
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		// Users should be incremented up to 100,000, and test finishes within 15
 		// minutes
-		InternalTestHelper.setInternalUserNumber(100);
+		InternalTestHelper.setInternalUserNumber(100000);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
-		List<User> allUsers = new ArrayList<>();
-		allUsers = tourGuideService.getAllUsers();
+		List<User> allUsers = tourGuideService.getAllUsers();
 
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
+
+		List<CompletableFuture<VisitedLocation>> futures = new ArrayList<>();
+
 		for (User user : allUsers) {
-			tourGuideService.trackUserLocation(user);
+			futures.add(tourGuideService.trackUserLocation(user));
 		}
+
+		CompletableFuture<Void> combinedFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+		combinedFutures.join();
+		// Avec ".allOf" on regroupe tous les CompletableFutures générés par les méthodes trackUserLocation.
+		// On attend que leur threads se termine avec ".join".
+
 		stopWatch.stop();
 		tourGuideService.tracker.stopTracking();
 
@@ -79,17 +89,22 @@ public class TestPerformance {
 
 		// Users should be incremented up to 100,000, and test finishes within 20
 		// minutes
-		InternalTestHelper.setInternalUserNumber(100);
+		InternalTestHelper.setInternalUserNumber(100000);
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 
 		Attraction attraction = gpsUtil.getAttractions().get(0);
-		List<User> allUsers = new ArrayList<>();
-		allUsers = tourGuideService.getAllUsers();
-		allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(), attraction, new Date())));
+		List<User> allUsers = tourGuideService.getAllUsers();
 
-		allUsers.forEach(u -> rewardsService.calculateRewards(u));
+		allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(), attraction, new Date())));
+		List<CompletableFuture<Void>> futuresCalculateRewards = new ArrayList<>();
+		allUsers.forEach(u -> futuresCalculateRewards.add(rewardsService.calculateRewards(u)));
+
+		CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(futuresCalculateRewards.toArray(new CompletableFuture[0]));
+		combinedFuture.join();
+		// Avec ".allOf" on regroupe tous les CompletableFutures générés par les méthodes calculateRewards.
+		//On attend que leur thread se termine avec ".join".
 
 		for (User user : allUsers) {
 			assertTrue(user.getUserRewards().size() > 0);
@@ -101,5 +116,4 @@ public class TestPerformance {
 				+ " seconds.");
 		assertTrue(TimeUnit.MINUTES.toSeconds(20) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 	}
-
 }
